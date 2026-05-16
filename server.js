18de064cfb37ec9d2209af0d49085d22e94d7736
Zeeshan <ez.zeeshanemmanuel@gmail.com>
2026-05-16 09:37:21 +0500

    STEP 3 - Authentication system (Register/Login with JWT)

--- server.js
+++ server.js
@@ -5,15 +5,30 @@ require("dotenv").config();
 
 const app = express();
 
+const authRoutes = require("./routes/AuthRoutes");
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected Successfully");
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed:", err);
+});
+
 // Test Route
 app.get("/", (req, res) => {
-
     res.status(200).json({ success: 'CRM API is running...' })
 });
+
+// Other Routes
+app.use("/api/auth", authRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
